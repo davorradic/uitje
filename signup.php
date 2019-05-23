15be69acc5e1231e0fcacc04174e7098e64368f2
davorradic <44604843+davorradic@users.noreply.github.com>
2019-05-23 08:49:21 +0200

    signup error under inputs

--- signup.php
+++ signup.php
@@ -5,36 +5,39 @@
     <?php
     var_dump($_POST);
     if(isset($_POST['send'])){
+      $errors = [];
         if(isset($_POST['gebruikersnaam']) && empty($_POST['gebruikersnaam'])){
-            echo 'vul gebruikersnaam in';
+            $errors['gebruikersnaam_error'] = 'vul gebruikersnaam in';
         }
         if(isset($_POST['gebruikersnaam']) && strlen($_POST['gebruikersnaam']) < 6){
-            echo 'gebruikersnaam moet meer dan 6 karakters zijn';
+            $errors['gebruikersnaammaxchar_error'] = 'gebruikersnaam moet meer dan 6 karakters zijn';
         }
         if(isset($_POST['wachtwoord']) && empty($_POST['wachtwoord'])){
-          echo 'vul wachtwoord in';
+          $errors['wachtwoord_error'] = 'vul wachtwoord in';
         }
         if(isset($_POST['wachtwoord']) && strlen($_POST['wachtwoord']) < 7){
-            echo 'wachtwoord moet meer dan 6 karakters zijn';
+          $errors['wachtwoordmaxchar_error'] = 'wachtwoord moet meer dan 6 karakters zijn';
         }
         if($_POST['wachtwoord'] != $_POST['herhaal_wachtwoord']){
-          echo 'de wachtwoorden komen niet overeen';
+          $errors['wachtwoordherhaal_error'] = 'de wachtwoorden komen niet overeen';
         }
         if(isset($_POST['email']) && empty($_POST['email']) ){
-          echo 'vul email in';
+          $errors['email_error'] = 'vul email in';
         }
         if(isset($_POST['email']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-          echo 'email is niet valid';
+          $errors['email_format_error'] = 'email is niet valid';
         }
         if(isset($_POST['voorletters']) && empty($_POST['voorletters'])){
-          echo 'vul voorletters in';
+          $errors['voorletters_error'] = 'vul voorletters in';
         }
         if(isset($_POST['tussenvoegsel']) && empty($_POST['tussenvoegsel'])){
-          echo 'vul tussenvoegsel in';
+          $errors['tussenvoegsel_error'] = 'vul tussenvoegsel in';
         }
         if(isset($_POST['achternaam']) && empty($_POST['achternaam'])){
-          echo 'vul achternaam in';
+          $errors['achternaam_error'] = 'vul achternaam in';
         }
+        
+
     }
 
 ?>
@@ -50,7 +53,8 @@
                 <label>Gebruikersnaam</label>
                 <input value="<?php echo (isset($_POST['gebruikersnaam']) ? $_POST['gebruikersnaam'] : '')?>" name="gebruikersnaam" class="form-control" id="gebruikersnaam" type="text" placeholder="Gebruikersnaam">
                 <p class="help-block text-danger">
-                    
+                    <?php echo (!empty($errors['gebruikersnaam_error']) ? $errors['gebruikersnaam_error'].'<br/>' : '');?>
+                    <?php echo (!empty($errors['gebruikersnaammaxchar_error']) ? $errors['gebruikersnaammaxchar_error'] : '');?>
                 </p>
               </div>
             </div>
@@ -58,14 +62,18 @@
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Wachtwoord</label>
                 <input value="<?php echo (isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : '')?>" name="wachtwoord" class="form-control" id="wachtwoord" type="password" placeholder="Wachtwoord">
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                <?php echo (!empty($errors['wachtwoord_error']) ? $errors['wachtwoord_error'] : '');?>
+                </p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Herhaal Wachtwoord</label>
                 <input value="<?php echo (isset($_POST['herhaal_wachtwoord']) ? $_POST['herhaal_wachtwoord'] : '')?>" name="herhaal_wachtwoord" class="form-control" id="herhaal_wachtwoord" type="password" placeholder="Herhaal Wachtwoord">
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                  <?php echo (!empty($errors['wachtwoordherhaal_error']) ? $errors['wachtwoordherhaal_error'] : '');?>
+                </p>
               </div>
             </div>
             
@@ -73,28 +81,37 @@
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Email Adres</label>
                 <input value="<?php echo (isset($_POST['email']) ? $_POST['email'] : '')?>" class="form-control" name="email" id="email" type="text" placeholder="Email Adres" >
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                  <?php echo (!empty($errors['email_error']) ? $errors['email_error'] : '');?><br />
+                  <?php echo (!empty($errors['email_format_error']) ? $errors['email_format_error'] : '');?>
+                </p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Voorletters</label>
                 <input value="<?php echo (isset($_POST['voorletters']) ? $_POST['voorletters'] : '')?>" class="form-control" id="voorletters" name="voorletters" type="text" placeholder="Voorletters">
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                  <?php echo (!empty($errors['voorletters_error']) ? $errors['voorletters_error'] : '');?>
+                </p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Tussenvoegsel</label>
                 <input value="<?php echo (isset($_POST['tussenvoegsel']) ? $_POST['tussenvoegsel'] : '')?>" name="tussenvoegsel" class="form-control" id="tussenvoegsel" type="text" placeholder="Tussenvoegsel">
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                  <?php echo (!empty($errors['tussenvoegsel_error']) ? $errors['tussenvoegsel_error'] : '');?>
+                </p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Achternaam</label>
                 <input value="<?php echo (isset($_POST['achternaam']) ? $_POST['achternaam'] : '')?>" class="form-control" id="achternaam" name="achternaam" type="text" placeholder="Achternaam">
-                <p class="help-block text-danger"></p>
+                <p class="help-block text-danger">
+                <?php echo (!empty($errors['achternaam_error']) ? $errors['achternaam_error'] : '');?>
+                </p>
               </div>
             </div>
             <br>
